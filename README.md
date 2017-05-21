373667ddb533f709f2d077e74850d60b2d8bfe72
Snipzwolf <none@none.none>
2017-05-21 18:53:48 +0100

    fixed typo in logs directory added example usage

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # solr
-solr docker image of version not provided by official image
+solr docker image of 3.6.2 as it's not provided by the [official image](https://hub.docker.com/_/solr/)
+
+## Example usage
+```
+docker run -d --restart=always \
+  -e REPLICATION_MASTER="true"
+  -e REPLICATION_SLAVE="false"
+  -e REPLICATION_HOST="127.0.0.1:8986"
+  -e JAVA_STACKSIZE="4m"
+  -e JAVA_HEAPSIZE="512m"
+  -e JAVA_PERMSIZE="24m"
+  -v /var/solr \
+	--name solr snipzwolf/solr
+
+```
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 su -c "java -server -classpath /opt/tomcat/bin/bootstrap.jar:/opt/tomcat/bin/tomcat-juli.jar -Dcatalina.home=/opt/tomcat -Djava.endorsed.dirs=/opt/tomcat/endorsed"\
-"   -Dsolr.solr.home=/etc/solr/ -Dcatalina.logs=/var/logs/solr/"\
+"   -Dsolr.solr.home=/etc/solr/ -Dcatalina.logs=/var/log/solr/"\
 "   -Djava.util.logging.config.file=/etc/solr/logging.properties -Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager"\
 "   -Xmx$JAVA_HEAPSIZE -Xms$JAVA_HEAPSIZE -Xss$JAVA_STACKSIZE -XX:PermSize=$JAVA_PERMSIZE"\
 "   -Dreplication.slave=$REPLICATION_SLAVE -Dreplication.master=$REPLICATION_MASTER"\
